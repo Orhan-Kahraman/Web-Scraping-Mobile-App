df3c75c0b6d7cacd401939edf3b12b8db3035218
Orhan-Kahraman <herorhan999@gmail.com>
2022-10-25 11:11:43 +0300

    Connect MongoDB

---
+++ lib/Model/pc_info_modal.dart
@@ -0,0 +1,24 @@
+class Bilgisayarin {
+  Bilgisayarin(
+      {this.ismi = "Belirtilmemiş",
+      this.linki = "Belirtilmemiş",
+      this.fiyati = "Belirtilmemiş",
+      this.islemciTipi = "Belirtilmemiş",
+      this.ssdKapasitesi = "Belirtilmemiş",
+      this.isletimSistemi = "Belirtilmemiş",
+      this.ekranKarti = "Belirtilmemiş",
+      this.ram = "Belirtilmemiş",
+      this.cozunurluk = "Belirtilmemiş",
+      this.ekranBoyutu = "Belirtilmemiş"});
+
+  String ismi;
+  String linki;
+  String fiyati;
+  String islemciTipi;
+  String ssdKapasitesi;
+  String isletimSistemi;
+  String ekranKarti;
+  String ram;
+  String cozunurluk;
+  String ekranBoyutu;
+}
